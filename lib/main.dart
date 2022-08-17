244750104e2f8c6e9d12be156591f7d871b86936
Max <samueladelowo92@gmail.com>
2022-08-17 16:05:24 +0100

    call successful

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:async';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const StarWars());
@@ -12,8 +15,90 @@ class StarWars extends StatefulWidget {
 }
 
 class _StarWarsState extends State<StarWars> {
+  final String url = "https://swapi.dev/api/starships";
+  List? data;
+
+  Future<String> getSWData() async {
+    var res =
+        await http.get(Uri.parse(url), headers: {"Accept": "application/json"});
+
+    setState(() {
+      var resBody = jsonDecode(res.body);
+      data = resBody["results"];
+    });
+
+    return "Success";
+  }
+
+  @override
+  void initState() {
+    this.getSWData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Star Wars Starships'),
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: ListView.builder(
+          itemCount: data == null ? 0 : data!.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Card(
+                      child: Container(
+                        child: Text(
+                          data![index]["name"],
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.lightBlue,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Container(
+                        child: Row(
+                          children: [
+                            Text('Name: '),
+                            Text(
+                              data![index]["model"],
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.lightBlue,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      child: Container(
+                        child: Row(
+                          children: [
+                            Text('Model: '),
+                            Text(
+                              data![index]["model"],
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.lightBlue,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
   }
 }
